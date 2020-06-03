2ca3769710d5190aa5eccc21b3a419732ab98420
Cleber Boni <bonifaciocleber@gmail.com>
2020-06-03 16:14:12 -0300

    uf, city from ibge

--- front/src/pages/CreatePoint/index.tsx
+++ front/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState, ChangeEvent} from 'react';
 import { Link } from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import axios from 'axios';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data);
+    })
+  }, []);
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/').then(response => {
+      const ufInitials = response.data.map(uf => uf.sigla);
+      setUfs(ufInitials);
+    })
+  }, []);
+
+  useEffect(() => {
+    if(selectedUf === '0'){
+      return;
+    }
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+      const cityNames = response.data.map(city => city.nome);
+      setCities(cityNames);
+    })
+  }, [selectedUf]);
+
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+    const uf = event.target.value;
+    setSelectedUf(uf);
+  }
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
+    const city = event.target.value;
+    setSelectedCity(city);
+  }
+
   return(
    <div id="page-create-point">
     <header>
@@ -42,17 +98,30 @@ const CreatePoint = () => {
           <h2>Endereço</h2>
           <span>Selecione o endereço no mapa</span>
         </legend>
+        <Map center={[-25.4425265, -49.2838927]} zoom={13}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-25.4425265, -49.2838927]}/>
+        </Map>
         <div className="field-group">
           <div className="field">
             <label htmlFor="uf">Estado (UF)</label>
-            <select name="uf" id="uf">
+            <select name="uf" id="uf" value={selectedUf} onChange={handleSelectUf}>
               <option value="0">Selecione um UF</option>
+              {ufs.map(uf => (
+                <option key={uf} value={uf}>{uf}</option>
+              ))}
             </select>
           </div>
           <div className="field">
             <label htmlFor="city">Cidade</label>
-            <select name="city" id="city">
+            <select name="city" id="city" value={selectedCity} onChange={handleSelectCity}>
               <option value="0">Selecione uma Cidade</option>
+              {cities.map(city => (
+                <option key={city} value={city}>{city}</option>
+              ))}
             </select>
           </div>
         </div>
@@ -63,30 +132,12 @@ const CreatePoint = () => {
           <span>Selecione um ou mais itens abaixo</span>
         </legend>
         <ul className="items-grid">
-          <li>
-            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-            <span>Óleo de cozinha</span>
-          </li>
-          <li className="selected">
-            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-            <span>Óleo de cozinha</span>
-          </li>
-          <li>
-            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-            <span>Óleo de cozinha</span>
-          </li>
-          <li>
-            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-            <span>Óleo de cozinha</span>
-          </li>
-          <li>
-            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-            <span>Óleo de cozinha</span>
-          </li>
-          <li>
-            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-            <span>Óleo de cozinha</span>
-          </li>
+          {items.map(item => (
+            <li key={item.id}>
+              <img src={item.image_url} alt={item.title}/>
+              <span>{item.title}</span>
+              </li>
+          ))}
         </ul>
       </fieldset>
       <button type="submit">
